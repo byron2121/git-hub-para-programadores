e8c0ace50e6f77f460bd48c6f9ceeffa94b2f73a
byron2121 <88109083+byron2121@users.noreply.github.com>
2021-07-28 14:12:30 -0500

    Create mi_archivo.py
    mi primer py

---
+++ mi_archivo.py
@@ -0,0 +1,9 @@
+print("**********************************************************************")
+print("este programa imprime una palabra y numero insertado desde el  teclado")
+print("********************************************************************** \n")
+
+palabra=input("introduce palabra")
+numero=int(input("introduce un numero entero"))
+
+print("string", palabra)
+print("numero entero", numero)
